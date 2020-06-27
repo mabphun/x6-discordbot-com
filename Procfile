9dcc023aac5d529f7c22172374ba75262a682041
X6parancsnok <33196966+X6parancsnok@users.noreply.github.com>
2020-06-27 10:07:23 +0200

    First
    aa

---
+++ Procfile
@@ -0,0 +1,1 @@
+Worker: dotnet Program.cs
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DCBOT_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
