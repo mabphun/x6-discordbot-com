b0bedfd6d7afcdf1000d15c104dcd232d1e0a188
X6parancsnok <33196966+X6parancsnok@users.noreply.github.com>
2020-07-01 00:02:45 +0200

    Base update

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+var token = "";
+
+const prefix ="!";
+
 client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login(process.env.token);
+client.on("message", message => {
+   if(message.content.startsWith(prefix + "ping")){
+    message.channel.send("pong");
+   }
+   
+});
+
+client.on("message", message => {
+    if(message.content.startsWith(prefix + "avatar")){
+        message.reply(message.author.avatarURL());
+    }
+})
+
+const fs = require("fs");
+const tokenFilePath = "./token.js";
+
+
+function login(){
+    if (fs.existsSync(tokenFilePath)){
+        var tokenFile = require("./token");
+        token = tokenFile.token;
+    }
+    else{
+        token = process.env.token;
+    }
+    client.login(token);
+}
+
+login();
